670d5e92e803c12b3257282d3ffef74ce4611ccb
Ryugaikiru <77698156+Ryugaikiru@users.noreply.github.com>
2021-01-21 12:03:51 -0300

    文を修正（sintax）

--- discordbot.py
+++ discordbot.py
@@ -7,15 +7,13 @@ import random
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-
 def randomcrab():
     crab = ['ズワイガニ', 'ガザミ', 'トラフカラッパ', 'エンコウガニ', 'ヒシガニ', 'ケガニ', 'ベニイワガニ', 'モズクガニ', 'アサヒガニ', 'ロブスター', 'ダンジネスクラブ', 'クリスマスアカガニ', 'ノコギリガザミ', 'オオホモラ', 'ヤシガニ', 'タラバガニ', 'オオカイカムリ', 'タカアシガニ', 'メタルクラブ', 'タスマニアオオガニ', 'シャコ', 'シオマネキ', 'ハナサキガニ']
     return random.choice(crab)
 def randomweapon():
-    weapon = { ステゴロ: '片手', ナイフ: '片手', ソード: '片手', カタナ: '片手', ショーテル: '片手', レイピア: '片手', ハンドアックス: '片手', ダガー: '片手', カタール: '片手', コチョウトウ: '片手', ビームセイバー: '片手', ヘビーソード: '片手', ジャバラケン: '片手', ジャベリン: '片手', ランス: '片手', トライデント: '片手', バール: '片手', バット: '片手', メイス: '片手', ウォーハンマー: '片手', トンファー: '片手', フレイル: '片手', ヌンチャク: '片手', ガーダ: '片手', リボルバー: '片手', ショットガン: '片手', アックスガン: '片手', サイ: '片手', シールド: '片手', クサリガマ: '片手', ジェット: '片手', アンカー: '片手', テッセン: '片手', ブーメラン: '片手', ドリル: '片手', パイルバンカー: '片手', クレイモア: '両手', グアンダオ: '両手', ハルバード: '両手', チェーンソー: '両手', グレートハンマー: '両手', ノダチ: '両手', ダブルセイバー: '両手', エクスカニバー: '両手', バルディッシュ: '両手', サンセツコン: '両手', アザラシ: 'のりもの', スクーター: 'のりもの' };
+    weapon = {ステゴロ: '片手', ナイフ: '片手', ソード: '片手', カタナ: '片手', ショーテル: '片手', レイピア: '片手', ハンドアックス: '片手', ダガー: '片手', カタール: '片手', コチョウトウ: '片手', ビームセイバー: '片手', ヘビーソード: '片手', ジャバラケン: '片手', ジャベリン: '片手', ランス: '片手', トライデント: '片手', バール: '片手', バット: '片手', メイス: '片手', ウォーハンマー: '片手', トンファー: '片手', フレイル: '片手', ヌンチャク: '片手', ガーダ: '片手', リボルバー: '片手', ショットガン: '片手', アックスガン: '片手', サイ: '片手', シールド: '片手', クサリガマ: '片手', ジェット: '片手', アンカー: '片手', テッセン: '片手', ブーメラン: '片手', ドリル: '片手', パイルバンカー: '片手', クレイモア: '両手', グアンダオ: '両手', ハルバード: '両手', チェーンソー: '両手', グレートハンマー: '両手', ノダチ: '両手', ダブルセイバー: '両手', エクスカニバー: '両手', バルディッシュ: '両手', サンセツコン: '両手', アザラシ: 'のりもの', スクーター: 'のりもの'}
     names = list(weapon)
-    r = random.choice names
+    r = random.choice(names)
     rtype = weapon[r]
     weaponl = []
     if rtype == '片手':
@@ -28,13 +26,13 @@ def randomweapon():
                 weaponl.append(names[num])
     else:
         weaponl.append(r)
-    l = random.choice weaponl
-    return [r,l]
+    larm = random.choice weaponl
+    return [r, larm]
 def shuffling():
-    if random.int(0,1) == 0:
-        weaponset = ('右手：' + sayuu[0] + '　左手：' + sayuu[1]);
+    if random.int(0, 1) == 0:
+        weaponset = ('右手：' + sayuu[0] + '　左手：' + sayuu[1])
     else:
-        weaponset = ('右手：' + sayuu[1] + '　左手：' + sayuu[0]);
+        weaponset = ('右手：' + sayuu[1] + '　左手：' + sayuu[0])
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
@@ -64,19 +62,19 @@ async def knsin(ctx):
     weaponset = 0
     shuffling()
     brandomizer = '\nホスト：' + randomcrab() + '\n' + weaponset
-    shuffling();
-    brandomizer += '\n' + '\n' + 'ゲスト：' + randomcrab() + '\n' + weaponset;
+    shuffling()
+    brandomizer += '\n' + '\n' + 'ゲスト：' + randomcrab() + '\n' + weaponset
     await ctx.send(brandomizer)
 async def kntag(ctx):
     weaponset = 0
     shuffling()
     drandomizer = '\nホスト：' + randomcrab() + '\n' + weaponset
-    shuffling();
-    drandomizer += '\n' + '\n' + 'ホストのパートナー：' + randomcrab() + '\n' + weaponset;
-    shuffling();
-    drandomizer += '\n' + '\n' + 'ホストの対面：' + randomcrab() + '\n' + weaponset;
-    shuffling();
-    drandomizer += '\n' + '\n' + '対面のパートナー：' + randomcrab() + '\n' + weaponset;
+    shuffling()
+    drandomizer += '\n' + '\n' + 'ホストのパートナー：' + randomcrab() + '\n' + weaponset
+    shuffling()
+    drandomizer += '\n' + '\n' + 'ホストの対面：' + randomcrab() + '\n' + weaponset
+    shuffling()
+    drandomizer += '\n' + '\n' + '対面のパートナー：' + randomcrab() + '\n' + weaponset
     await ctx.send(drandomizer)
 
 bot.run(token)
